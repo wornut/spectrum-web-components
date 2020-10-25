f55f2927d314b38f1cb923932825b1e49d939930
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-10-25 13:03:28 -0400

    feat(color-wheel): add color-wheel pattern visuals

---
+++ packages/color-wheel/src/ColorWheel.ts
@@ -0,0 +1,61 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    html,
+    SpectrumElement,
+    CSSResultArray,
+    TemplateResult,
+    property,
+} from '@spectrum-web-components/base';
+
+import styles from './color-wheel.css.js';
+import { wheel } from './wheel-svg.js';
+
+/**
+ * @element sp-color-wheel
+ */
+export class ColorWheel extends SpectrumElement {
+    public static get styles(): CSSResultArray {
+        return [styles];
+    }
+
+    @property({ type: Boolean, reflect: true })
+    public disabled = false;
+
+    @property({ type: String })
+    public color = 'rgb(255, 0, 0)';
+
+    protected render(): TemplateResult {
+        return html`
+            <slot name="gradient">
+                ${wheel}
+            </slot>
+
+            <sp-color-handle
+                class="handle"
+                color=${this.color}
+                ?disabled=${this.disabled}
+                style="transform: translate(67.5px, 0);"
+            ></sp-color-handle>
+
+            <input
+                type="range"
+                class="slider"
+                aria-label="hue"
+                min="0"
+                max="360"
+                step="1"
+            />
+        `;
+    }
+}
