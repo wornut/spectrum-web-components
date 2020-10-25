d27c02a49412f8a0dc6befc18692aa036333b094
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-10-25 13:07:02 -0400

    feat(color-area): add color-area pattern visuals

---
+++ packages/color-area/src/ColorArea.ts
@@ -0,0 +1,73 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    html,
+    SpectrumElement,
+    CSSResultArray,
+    TemplateResult,
+    property,
+} from '@spectrum-web-components/base';
+
+import styles from './color-area.css.js';
+
+/**
+ * @element sp-color-area
+ */
+export class ColorArea extends SpectrumElement {
+    public static get styles(): CSSResultArray {
+        return [styles];
+    }
+
+    @property({ type: Boolean, reflect: true })
+    public disabled = false;
+
+    @property({ type: String })
+    public color = 'rgb(255, 0, 0)';
+
+    protected render(): TemplateResult {
+        return html`
+            <div
+                class="gradient"
+                style="background: linear-gradient(to top, black 0%, rgba(0, 0, 0, 0) 100%), linear-gradient(to right, white 0%, rgba(0, 0, 0, 0) 100%), rgb(255, 0, 0);"
+            >
+                <slot name="gradient"></slot>
+            </div>
+
+            <sp-color-handle
+                class="handle"
+                color=${this.color}
+                ?disabled=${this.disabled}
+                style="transform: translate(192px, 0px);"
+            ></sp-color-handle>
+
+            <input
+                type="range"
+                class="slider"
+                name="x"
+                aria-label="saturation and value"
+                min="0"
+                max="1"
+                step="0.01"
+            />
+            <input
+                type="range"
+                class="slider"
+                name="y"
+                aria-label="saturation and value"
+                min="0"
+                max="1"
+                step="0.01"
+            />
+        `;
+    }
+}
